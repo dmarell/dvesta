ac921fbb4c38cbc7d510f4b6d29d7f0cd0c00a81
Daniel Marell <daniel.marell@gmail.com>
2016-01-10 14:48:24 +0100

    Speech: Added @Qualifier slack to TextChatConnection.

--- src/main/java/se/marell/dvesta/speech/impl/SpeechControl.java
+++ src/main/java/se/marell/dvesta/speech/impl/SpeechControl.java
@@ -8,6 +8,7 @@ import com.sun.speech.freetts.VoiceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import se.marell.dvesta.chat.chatapi.TextChatConnection;
 import se.marell.dvesta.speech.TextToSpeech;
@@ -22,6 +23,7 @@ public class SpeechControl implements TextToSpeech, ServletContextListener {
     private Voice voice;
 
     @Autowired
+    @Qualifier("slack")
     private TextChatConnection chatConnection;
 
     @Override
